1ca3a38b3aa09a426249bd08ec3c867490ce40f9
JangHwanPark <wkd5878@gmail.com>
2024-04-10 10:18:25 +0900

    Feat : db 유틸함수, dao 클래스 분리

--- Java-Swing Register/src/Model/UserDAO.java
+++ Java-Swing Register/src/Model/UserDAO.java
@@ -1,28 +1,15 @@
 package Model;
 
+import Utils.ConnectDB;
 import java.sql.*;
 
 public class UserDAO {
-    // Test
-    public static void main(String[] args) {
-        String url = System.getenv("DB_URL");
-        String user = System.getenv("DB_USER");
-        String password = System.getenv("DB_PASS");
-
-        // Debuglog
-        System.out.println(url);
-        System.out.println(user);
-        System.out.println(password);
-
+    public void selectUser() {
+        // DB 유틸함수 호출
+        Connection conn = ConnectDB.initDB();
+        System.out.println(ConnectDB.initDB());
         try {
-            // 데이터베이스 드라이버 클래스 로드
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-            // 데이터베이스에 연결
-            Connection conn = DriverManager.getConnection(url, user, password);
-            if (conn != null) { // Success: 연결 성공
-                System.out.println("데이터베이스에 성공적으로 연결되었습니다.");
-
+            if (conn != null) {
                 // Test: Query
                 String sql = "SELECT * FROM java_swing_register_member";
 
@@ -32,14 +19,22 @@ public class UserDAO {
                 // Test: Query Start
                 ResultSet resultSet = statement.executeQuery(sql);
                 System.out.println("data: " + resultSet);
-                conn.close(); // 데이터베이스 연결 종료
+
+                resultSet.close();  // 쿼리 결과를 담고 있는 ResultSet 종료
+                statement.close();  // SQL 실행을 위한 Statement 객체 종료
+                conn.close();       // 데이터베이스 연결 종료
+            } else {
+                System.out.println("데이터베이스 연결에 실패했습니다.");
             }
-        } catch (ClassNotFoundException e) { // Fail: 연결 실패
-            System.out.println("MySQL JDBC 드라이버를 찾을 수 없습니다.");
-            e.printStackTrace();
-        } catch (SQLException e) {
-            System.out.println("데이터베이스 연결에 실패했습니다.");
+        } catch (Exception e) {
+            System.out.println("오류 발생");
             e.printStackTrace();
         }
+    }
+
+    // Test
+    public static void main(String[] args) {
+        UserDAO u = new UserDAO();
+        u.selectUser();
     }
 }
---
+++ Java-Swing Register/src/Utils/ConnectDB.java
@@ -0,0 +1,36 @@
+package Utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectDB {
+    public static Connection initDB() {
+        Connection conn;
+
+        try {
+            // 데이터베이스 드라이버 등록 (MySQL JDBC Driver)
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // DB 연결
+            conn = DriverManager.getConnection(
+                    System.getenv("DB_URL"),
+                    System.getenv("DB_USER"),
+                    System.getenv("DB_PASS")
+            );
+
+            if (conn != null) { // Success: 연결 성공
+                System.out.println("데이터베이스에 성공적으로 연결되었습니다.");
+                return conn;
+            }
+        } catch (ClassNotFoundException e) { // Fail: 연결 실패
+            System.out.println("MySQL JDBC 드라이버를 찾을 수 없습니다.");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("데이터베이스 연결에 실패했습니다.");
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
